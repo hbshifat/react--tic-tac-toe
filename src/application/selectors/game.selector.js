5901d47dabd8b306992f499bbc8b17e6251002fe
Hasanul Banna Shifat <16060768+hbshifat@users.noreply.github.com>
2021-07-31 20:57:41 +0600

    update

--- src/application/selectors/game.selector.js
+++ src/application/selectors/game.selector.js
@@ -1,4 +1,8 @@
 // @Selectors
+
+// get history array
 export const getHistory = ({ gameReducer }) => gameReducer.History;
+// get Step number
 export const getStep = ({ gameReducer }) => gameReducer.Step;
+// get next player status
 export const getNextPlayer = ({ gameReducer }) => gameReducer.NextPlayer;
